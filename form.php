32d540347bc18655a076ab194023908bd49f4c20
Bruno <brunosca96@gmail.com>
2025-02-12 10:02:52 -0800

    segundo commit en el form.php

--- form.php
+++ form.php
@@ -8,7 +8,7 @@
 <body>
     <h2>Welcome to the Interactive Treasure Hunt!</h2>
     <form action="process.php" method="post">
-        <label for="number">Enter a Number (e.g., birth year):</label>
+        <label for="number">Enter a Number! (e.g., birth year):</label>
         <input type="number" name="number" required><br><br>
 
         <label for="text">Enter a Text Message:</label>
